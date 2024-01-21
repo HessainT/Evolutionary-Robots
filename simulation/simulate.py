c390853348bb822bcfc192ee054c14449195379d
Hessain Tamang <hessain@Hessains-MacBook-Pro.local>
2024-01-21 16:14:40 -0500

    Stepping the simulation

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -1,11 +1,20 @@
-# Import pybullet
+# Import stuff
 import pybullet as p
+import time
 
 # Create an object to handle and draw results to GUI
 physicsClient = p.connect(p.GUI)
 
+# OPTIONAL: Disable sidebars
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+# Stepping the simulation
+# Loop through the action
+for i in range(1000): 
+    p.stepSimulation() # Step
+    time.sleep(1/60) # Sleep for 1/60 second
+    print(i)
+
 p.disconnect #Disconnect object
-
-
 
 
